6a1f9d744a92b8ccedd53f2bd712415f94d5ccce
Erwin de Keijzer <erwin@dekeijzer.xyz>
2018-04-24 15:21:51 +0200

    updated build job

--- brigade.js
+++ brigade.js
@@ -1,5 +1,24 @@
-const { events } = require("brigadier");
+const { events, Job } = require("brigadier");
 
 events.on("push", function(e, project) {
   console.log("received push for commit " + e.commit)
+
+  // Create a new job
+  var node = new Job("test-runner")
+
+  // We want our job to run the stock Docker Python 3 image
+  node.image = "golang:1.9.4-alpine3.7"
+
+  node.tasks = [
+    "mkdir -p /go/src/github.com/gnur/",
+    "cp -a /src/ /go/src/github.com/gnur/booksing/",
+    "cd /go/src/github.com/gnur/booksing/",
+    "go get github.com/jteeuwen/go-bindata/...",
+    "go get github.com/elazarl/go-bindata-assetfs/...",
+    "go-bindata-assetfs static/...",
+    "RUN go build -o app *.go"
+  ]
+
+  // We're done configuring, so we run the job
+  node.run()
 })
