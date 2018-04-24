fbb1e408633f06bb7d7d3f71c2cb23ad3ef828ae
Erwin de Keijzer <erwin@dekeijzer.xyz>
2018-04-24 15:30:30 +0200

    bogus

--- brigade.js
+++ brigade.js
@@ -1,6 +1,7 @@
 const { events, Job } = require("brigadier");
 
 events.on("push", function(e, project) {
+
   console.log("received push for commit " + e.commit)
 
   var node = new Job("test-runner")
